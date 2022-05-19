4295bf2f73428248a2485e6c39602726ba2272b6
Juliana Tagami <julianatagami@Julianas-MBP.lan>
2022-05-19 18:45:22 +0100

    ts-estudos

---
+++ app/models/negociacao.ts
@@ -0,0 +1,24 @@
+export class Negociacao {
+    //#nao pode alterar dados informacao 
+    constructor (
+        private _data: Date,
+        private _quantidade: number,
+        private _valor: number,
+    ) {}
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
